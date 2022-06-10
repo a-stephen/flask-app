90102f8f32582a1b93c664eebf8bdf436b813bed
Stephen Adjignon <s.adjignon015@hotmail.com>
2022-06-10 15:25:41 +0000

    add get_imagesnames function

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+import os
+import random
 
 app = Flask(__name__)
 
+
 @app.route('/')
+
+def get_imagesnames(path):
+    for imagesdir, imagesfolder, imagesnames in os.walk(path):
+        return imagesnames
+
+
 def index():
+    imgs = get_imagesnames("./static/images")
+    random_images = random.choice(imgs)
     return render_template("index.html")
 
 if __name__ == "__main__":
